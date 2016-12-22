52cbcbac6499da7108ed4208d03e4f314b943374
Administrator <online>
2016-12-22 12:45:02 +0800

    12

--- FunctionTest/FT09_Expenses/FT0905_PaymentApply.py
+++ FunctionTest/FT09_Expenses/FT0905_PaymentApply.py
@@ -167,6 +167,75 @@ class PaymentApply(unittest.TestCase):
                 print(i.text)
                 unittest.expectedFailure("test_0905_03_draft")
 
+    """费用管理-付款申请-普通款项申请添加功能"""
+    def test_0905_04_addNormal(self):
+        """费用管理-付款申请-普通款项申请添加功能"""
+        driver = self.driver
+        driver.find_element_by_id("BtnAdd").click()
+        time.sleep(3)
+        driver.switch_to_frame("winSendInspection_IFrame")        # 切换到新增页面
+        v_tim = time.strftime("%y%m%d%H%M")
+        driver.find_element_by_xpath("//*[@id='PartnerNum_Container']/div/span/img[2]").click()        # 收款单位
+        time.sleep(3)
+        driver.switch_to_frame("winAdd_IFrame")         # 切换到收款单位选择窗体
+        driver.find_element_by_id("txtSearchText").send_keys("v1")
+        driver.find_element_by_id("btnSearch").click()
+        time.sleep(1)
+        v_typelist = driver.find_elements_by_class_name("x-grid3-row")
+        v_typelist[random.randint(0, (len(v_typelist)-1))].click()
+        driver.find_element_by_id("btnSelect").click()
+        time.sleep(1)
+        driver.switch_to.parent_frame()
+        # 开户银行
+        driver.find_element_by_id("txtBank").send_keys(fun_data_bank())
+        # 银行账号
+        driver.find_element_by_id("txtBankNumber").send_keys(fun_idcard())
+        # 开户姓名
+        driver.find_element_by_id("txtBankName").send_keys(fun_data_name())
+        # 申请事由
+        driver.find_element_by_id("Remark").send_keys(
+            "这是一张付款申请单Auto，这是测试说的Auto。申请日期：" + v_tim)
+        # 用款类型选择
+        driver.find_element_by_id("cbxDocType_Container").click()
+        for i in driver.find_elements_by_class_name("x-combo-list-item"):
+            if i.text == "采购业务款项申请":
+                i.click()
+                break
+        time.sleep(2)
+        # 行数据选择
+        v_object = driver.find_element_by_xpath(
+            "//*[@id='GridPanel1']/div/div/div/div/div[2]/div/div/table/tbody/tr[1]/td[3]").click()
+        ActionChains(driver).double_click(v_object).perform()
+        time.sleep(1)
+        driver.find_element_by_xpath(
+            "//*[@id='GridPanel1']/div/div/div/div/div[2]/div[2]/div/span").click()
+        time.sleep(2)
+        driver.switch_to_frame("winAdd_IFrame")     # 切换到物料选择窗体
+        v_wlcode = driver.find_elements_by_class_name("x-grid3-row")
+        if len(v_wlcode) > 5:
+            v_total = 0
+            while v_total <= 4:
+                v_wlcode[v_total].click()
+                v_total += 1
+        else:
+            v_wlcode[0].click()
+        driver.find_element_by_id("Button1").click()
+        time.sleep(2)
+        driver.switch_to.parent_frame()
+        # 提交单据
+        driver.find_element_by_id("btnSave").click()
+        time.sleep(4)
+        v_tip = driver.find_elements_by_class_name("ext-mb-text")
+        for i in v_tip:
+            if "流程已触发" in i.text:
+                print(i.text)
+            elif "单据添加成功" in i.text:
+                print(i.text)
+            else:
+                driver.get_screenshot_as_file(root_path() + "TestPicture/test_0905_01_add.jpg")
+                print(i.text)
+                unittest.expectedFailure("test_0905_01_addNormal")
+
     def tearDown(self):
         self.driver.quit()
         self.assertEqual([], self.verificationErrors)
