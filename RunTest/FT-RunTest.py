2ff43d6ba1621e6a4ffd7b94513d2e165694d0a6
Administrator <online>
2017-01-10 17:04:32 +0800

    123

--- RunTest/FT-RunTest.py
+++ RunTest/FT-RunTest.py
@@ -7,21 +7,23 @@ from PubliCode.randData import root_path
 指定测试用例为当前文件夹下的test_case目录
 通过自定函数获取当前文件所在路径
 '''
+v_tim = time.strftime("%Y%m%d")
 test_dir = root_path() + "FunctionTest"
 discover = unittest.defaultTestLoader.discover(
     test_dir,
     pattern='FT*.py'
 )
 
+# 报告文件存放路径
+FileName = root_path() + 'TestReport/FTRport/' + v_tim + 'FT_online.htm'
+fp = open(FileName, 'wb')
+runner = HTMLTestRunner(
+    stream=fp,
+    title='online功能集成自动化测试',
+    description='online自动化测试——主流程功能测试执行结果统计'
+)
+runner.run(discover)
+fp.close()
+
 if __name__ == '__main__':
-    v_tim = time.strftime("%Y%m%d")
-    # 报告文件存放路径
-    FileName = root_path() + 'TestReport/FTRport/' + v_tim + 'FT_online.htm'
-    fp = open(FileName, 'wb')
-    runner = HTMLTestRunner(
-        stream=fp,
-        title='online功能集成自动化测试',
-        description='online自动化测试——主流程功能测试执行结果统计'
-    )
-    runner.run(discover)
-    fp.close()
+    print('测试结果')
