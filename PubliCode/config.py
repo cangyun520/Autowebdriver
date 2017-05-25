8623c501cc220847944b2edf375a9bfc191db43a
Administrator <online>
2017-05-25 14:23:33 +0800

    New changelist

--- PubliCode/config.py
+++ PubliCode/config.py
@@ -1,7 +1,12 @@
+import time
+import random
+import os
+import csv
 
-from uiautomator import device as driver
-import unittest
 
-# 打开weixin App
-def setUp(self):
-    driver.press.home()
+# 获取当前项目根目录
+def root_path(prjname="Autowebdriver"):
+    v_path = os.getcwd()
+    v_findpath = prjname
+    v_thepath = v_path[:v_path.find(v_findpath)] + v_findpath + "/"
+    return v_thepath
