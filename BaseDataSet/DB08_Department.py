3f67699f78696b321921fd530093136a1f9cf548
Administrator <online>
2017-06-13 17:06:31 +0800

    1

--- BaseDataSet/DB08_Department.py
+++ BaseDataSet/DB08_Department.py
@@ -16,7 +16,7 @@ class DB08_Department(unittest.TestCase):
     '''系统初始化-用户管理-部门管理'''
     def test_0801_Department(self):
         driver = self.driver
-        ClasMenu.menu_full_text(self, "人事管理", "用户管理", "部门管理")
+        ClasMenu.menu_full_text(self, "系统管理", "用户设置", "部门管理")
         # 移动到页面顶部，防止对象遮挡
         ClasForm.form_top(self, 0)
         driver.switch_to.frame("frame_tab_PM000006")
--- BaseDataSet/DB09_LineCustom.py
+++ BaseDataSet/DB09_LineCustom.py
@@ -13,25 +13,20 @@ class DB08_Department(unittest.TestCase):
         # 移动到页面顶部，防止对象遮挡
         ClasForm.form_top(self, 0)
 
-    '''系统初始化-用户管理-部门管理'''
+    '''系统管理-常规设置-行格式自定义'''
 
     def test_0801_Department(self):
         driver = self.driver
-        ClasMenu.menu_full_text(self, "人事管理", "用户管理", "部门管理")
-        # 移动到页面顶部，防止对象遮挡
-        ClasForm.form_top(self, 0)
-        driver.switch_to.frame("frame_tab_PM000006")
-        v_depart = driver.find_elements_by_class_name("x-treegrid-col")
-        v_total = 0
-        while v_total <= 4:
-            v_depart[v_total].click()
-            v_total += 1
-        driver.find_element_by_id("btnSelect").click()
+        ClasMenu.menu_full_text(self, "系统管理", "常规设置", "行格式自定义")
+        driver.switch_to.frame("frame_tab_PM000255")
+
+        driver.find_element_by_id("comboBoxXmlName").click()
+
+
 
     def tearDown(self):
         self.driver.quit()
         self.assertEqual([], self.verificationErrors)
-
 
 if __name__ == "__main__":
     unittest.main()
