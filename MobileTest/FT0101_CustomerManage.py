11d4e6b7b33d2f1d1144452b02602b71bede0725
Administrator <online>
2017-06-15 16:00:20 +0800

    应收+付款增加  付款方式数据查看

--- MobileTest/FT0101_CustomerManage.py
+++ MobileTest/FT0101_CustomerManage.py
@@ -11,14 +11,20 @@ class CustomerManage(unittest.TestCase):
         desired_caps = DingPublic.start_ding(self)
         self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
         self.driver.implicitly_wait(25)
-
-    def test_0101_01_check(self):
-        driver = self.driver
-        # driver.find_element_by_name("联系人").click()
         try:
-            driver.find_element_by_name("工作").click()
+            self.driver.find_element_by_name("工作").click()
         except Exception as err:
             print(err)
+
+    """钉钉-业务管理首页-页面检查"""
+    def test_0101_01_check(self):
+        """钉钉-业务管理首页-页面检查"""
+        driver = self.driver
+        # driver.find_element_by_name("联系人").click()
+        # try:
+        #     driver.find_element_by_name("工作").click()
+        # except Exception as err:
+        #     print(err)
 
         driver.find_element_by_name("业务管理首页").click()
         time.sleep(5)
@@ -37,13 +43,17 @@ class CustomerManage(unittest.TestCase):
             driver.get_screenshot_as_file(root_path() + "TestPicture/Ding/test_0101_01_check.jpg")
             unittest.expectedFailure("test_0101_01_check")
 
-    def test_0101_02_add(self):
+    """钉钉-业务管理首页-客户管理添加"""
+
+    def test_0101_02_CustomerAdd(self):
+        """钉钉-业务管理首页-客户管理添加"""
         driver = self.driver
         # driver.find_element_by_name("联系人").click()
-        try:
-            driver.find_element_by_name("工作").click()
-        except Exception as err:
-            print(err)
+        # try:
+        #     driver.find_element_by_name("工作").click()
+        # except Exception as err:
+        #     print(err)
+
         driver.find_element_by_name("业务管理首页").click()
         time.sleep(5)
         v_tim = time.strftime("%y%m%d%H%M%S")
@@ -60,8 +70,8 @@ class CustomerManage(unittest.TestCase):
         driver.find_element_by_accessibility_id("名称").click()
         driver.find_element_by_accessibility_id("名称").send_keys("客户DD" + v_tim + "有限公司")
         driver.find_element_by_accessibility_id("外文名称").send_keys(fun_data_englishname())
-        driver.find_element_by_accessibility_id("移动电话").send_keys(fun_data_mobile())
         driver.find_element_by_accessibility_id("电话").send_keys("027-" + str(random.randint(111111, 999999)))
+        driver.find_element_by_accessibility_id("移动电话").send_keys(fun_data_mobile())
         driver.find_element_by_accessibility_id("传真").send_keys("027-" + str(random.randint(111111, 999999)))
         driver.find_element_by_accessibility_id("电子邮件").send_keys(fun_data_email())
         driver.find_element_by_accessibility_id("Web站点").send_keys(fun_data_www())
@@ -70,7 +80,7 @@ class CustomerManage(unittest.TestCase):
         time.sleep(1)
         driver.find_element_by_accessibility_id("备注").send_keys("Python应用于钉钉端自动添加数据" + v_tim)
         driver.find_element_by_accessibility_id("添加 Link").click()
-        time.sleep(12)
+        time.sleep(8)
 
         # e 检测对象是否存在
         v_tip = "保存成功，单号：" + v_num
@@ -79,8 +89,137 @@ class CustomerManage(unittest.TestCase):
             print(v_tip)
         except Exception as err:
             print(err)
-            driver.get_screenshot_as_file(root_path() + "TestPicture/Ding/test_0101_02_add.jpg")
-            unittest.expectedFailure("test_0101_02_add")
+            driver.get_screenshot_as_file(root_path() + "TestPicture/Ding/test_0101_02_CustomerAdd.jpg")
+            unittest.expectedFailure("test_0101_02_Customer")
+
+    """钉钉-业务管理首页-供应商添加"""
+
+    def test_0101_03_SupplierAdd(self):
+        """钉钉-业务管理首页-供应商添加"""
+        driver = self.driver
+        driver.find_element_by_name("业务管理首页").click()
+        time.sleep(5)
+        v_tim = time.strftime("%y%m%d%H%M%S")
+        v_an = driver.find_elements_by_class_name("android.view.View")
+        v_an[4].click()
+        time.sleep(3)
+        driver.find_element_by_accessibility_id(" Link").click()
+
+        # 进入添加页面
+
+        driver.find_element_by_accessibility_id("代码*").click()
+        v_num = "VD" + str(random.randint(11111, 99999))
+        driver.find_element_by_accessibility_id("代码*").send_keys(v_num)
+        driver.find_element_by_accessibility_id("名称").click()
+        driver.find_element_by_accessibility_id("名称").send_keys("供应商DD" + v_tim + "有限公司")
+        driver.find_element_by_accessibility_id("外文名称").send_keys(fun_data_englishname())
+        driver.find_element_by_accessibility_id("电话").send_keys("017-" + str(random.randint(111111, 999999)))
+        driver.find_element_by_accessibility_id("移动电话").send_keys(fun_data_mobile())
+        driver.find_element_by_accessibility_id("传真").send_keys("017-" + str(random.randint(111111, 999999)))
+        driver.find_element_by_accessibility_id("电子邮件").send_keys(fun_data_email())
+        driver.find_element_by_accessibility_id("Web站点").send_keys(fun_data_www())
+        # 上下滑动屏幕
+        driver.swipe(0, 0, 0, 100)
+        driver.find_element_by_accessibility_id("备注").click()
+        time.sleep(1)
+        driver.find_element_by_accessibility_id("备注").send_keys("Python应用于钉钉端自动添加供应商数据" + v_tim)
+        driver.find_element_by_accessibility_id("添加 Link").click()
+        time.sleep(8)
+
+        # e 检测对象是否存在
+        v_tip = "保存成功，单号：" + v_num
+        try:
+            driver.find_element_by_accessibility_id(v_tip).is_displayed()
+            print(v_tip)
+        except Exception as err:
+            print(err)
+            driver.get_screenshot_as_file(root_path() + "TestPicture/Ding/test_0101_03_SupplierAdd.jpg")
+            unittest.expectedFailure("test_0101_03_SupplierAdd")
+
+    """钉钉-业务管理首页-库存查询检查"""
+
+    def test_0101_04_InventoryLook(self):
+        """钉钉-业务管理首页-库存查询检查"""
+        driver = self.driver
+        driver.find_element_by_name("业务管理首页").click()
+        time.sleep(5)
+        v_an = driver.find_elements_by_class_name("android.view.View")
+        v_an[5].click()
+        time.sleep(3)
+
+        # 进入查看页面
+
+        try:
+            driver.find_element_by_name("库存查询").is_displayed()
+        except Exception as err:
+            print(err)
+            driver.get_screenshot_as_file(root_path() + "TestPicture/Ding/test_0101_04_InventoryLook.jpg")
+            unittest.expectedFailure("test_0101_04_InventoryLook")
+
+        v_list = driver.find_elements_by_class_name("android.view.View")
+        v_list[1].click()
+        time.sleep(5)
+        # 进入物料查看详情页面
+
+        try:
+            driver.find_element_by_accessibility_id(" 返回").is_displayed()
+        except Exception as err:
+            print(err)
+            driver.get_screenshot_as_file(root_path() + "TestPicture/Ding/test_0101_04_InventoryLook.jpg")
+            unittest.expectedFailure("test_0101_04_InventoryLook")
+
+    """钉钉-业务管理首页-销售报价单添加"""
+
+    def test_0101_05_SalesOfferAdd(self):
+        """钉钉-业务管理首页-销售报价单添加"""
+        driver = self.driver
+        driver.find_element_by_name("业务管理首页").click()
+        time.sleep(5)
+        v_tim = time.strftime("%y%m%d%H%M%S")
+        v_an = driver.find_elements_by_class_name("android.view.View")
+        v_an[6].click()
+        time.sleep(5)
+        driver.find_element_by_accessibility_id(" Link").click()
+
+        # 进入添加页面
+        driver.find_element_by_accessibility_id("客户名称* Link").click()
+        time.sleep(2)
+        # 进入业务伙伴选择页面
+        v_list = driver.find_elements_by_class_name("android.view.View")
+        v_list[1].click()
+        time.sleep(1)
+        driver.find_element_by_accessibility_id("确定 Link").click()
+        time.sleep(2)
+
+        driver.find_element_by_accessibility_id("物料明细* Link").click()
+        time.sleep(2)
+        # 进入物料选择页面
+        v_list_wl = driver.find_elements_by_class_name("android.view.View")
+        v_list_wl[1].click()
+        driver.find_element_by_accessibility_id("提交 Link").click()
+        time.sleep(1)
+        driver.find_element_by_accessibility_id("添加物料明细项 Link").click()
+        v_list_wl[2].click()
+        driver.find_element_by_accessibility_id("提交 Link").click()
+        time.sleep(1)
+        driver.find_element_by_accessibility_id(" 返回 Link").click()
+        time.sleep(1)
+
+        # 上下滑动屏幕
+        driver.find_element_by_accessibility_id("备注").send_keys("Python应用于钉钉端自动添加销售报价单" + v_tim)
+        driver.swipe(0, 0, 0, 100)
+        driver.find_element_by_accessibility_id("添加 Link").click()
+        time.sleep(6)
+
+        # e 检测对象是否存在
+        v_tip = "保存成功"
+        try:
+            driver.find_element_by_accessibility_id(v_tip).is_displayed()
+            print(v_tip)
+        except Exception as err:
+            print(err)
+            driver.get_screenshot_as_file(root_path() + "TestPicture/Ding/test_0101_05_SalesOfferAdd.jpg")
+            unittest.expectedFailure("test_0101_05_SalesOfferAdd")
 
     def tearDown(self):
         self.driver.quit()
