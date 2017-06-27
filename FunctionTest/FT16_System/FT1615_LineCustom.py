b15f32eb936d0dfe8636c8674f22ca2be38e2b48
93 <405367236@qq.com>
2017-06-27 17:14:50 +0800

    93

--- FunctionTest/FT16_System/FT1615_LineCustom.py
+++ FunctionTest/FT16_System/FT1615_LineCustom.py
@@ -1,9 +1,9 @@
 from PubliCode.onlineClass import *
 from PubliCode.randData import *
-
+import re
 '''
     *   Arvin
-    *   2017-05-13
+    *   2017-06-23
 '''
 
 
@@ -13,7 +13,7 @@ class DataRelated(unittest.TestCase):
         ClasLogin.login_setup(self)
         driver = self.driver
         # 打开菜单
-        ClasMenu.menu_full_text(self, u"系统管理", u"常规设置", u"行格式自定义")
+        ClasMenu.menu_full_text(self, u"订货管理", u"系统管理", u"常规设置", u"行格式自定义")
         # 移动到页面顶部，防止对象遮挡
         ClasForm.form_top(self, 0)
         driver.switch_to.frame("frame_tab_PM000255")
@@ -22,103 +22,74 @@ class DataRelated(unittest.TestCase):
     def test_1615_01_AddFPGZ(self):
         """系统管理-警报设置-数据相关查询"""
         driver = self.driver
-        driver.find_element_by_id("comboBoxXmlName").clear()
-        n = 0
-        v_input = ['销售报价单(物料)', '销售报价单(服务)', '销售订单(物料)', '销售订单(服务)', "销售交货(物料)", "销售交货(服务)",
-                   "销售退货(物料)", "销售退货(服务)", '预收款申请(物料)', '预收款申请(服务)', '应收发票(物料)', '应收发票(服务)',
-                   '应收贷项凭证(物料)', '应收贷项凭证(服务)', '应收发票+付款(物料)', '应收预留发票(物料)', '应收预留发票(服务)',
-                   '采购申请(物料)', '采购申请(服务)', '采购报价单(物料)', '采购报价单(服务)', '采购订单(物料)', '采购订单(服务)',
-                   '采购收货(物料)', '采购收货(服务)', '采购退货(物料)', '采购退货(服务)', '应付发票(物料)', '应付发票(服务)',
-                   '预付款申请(物料)', '预付款申请(服务)', '应付贷项凭证(物料)', '应付贷项凭证(服务)', '应付预留发票(物料)',
-                   '应付预留发票(服务)']
-        print(len(v_input))
-        while n < len(v_input):
+        # 获取文件名称
+        driver.find_element_by_xpath(
+            "//*[@id='ext-comp-1002']/table/tbody/tr/td/table/tbody/tr/td/div/div/div/img").click()
+        time.sleep(1)
+        v_lists = driver.find_elements_by_class_name("x-combo-list-item")
+        v_meus = []
+        for i in v_lists:
+            if re.search(r'(物料)', i.text) or re.search(r'(服务)', i.text) or re.search(r'库存', i.text):
+                v_meus.append(i.text)
+            else:
+                pass
+
+        # 剔除多余数据
+        for j in v_meus:
+            l = ['调拨计划物料选择']
+            for d in l:
+                if d == j:
+                    v_meus.remove(j)
+        # print(v_meus)
+        # quit()
+        n = len(v_meus)
+        m = 0
+
+        # 开始循环
+        while m < n:
+            # 清理输入框
             driver.find_element_by_id("comboBoxXmlName").clear()
-            driver.find_element_by_id("comboBoxXmlName").send_keys(v_input[n])
-            print(v_input[n])
-            time.sleep(5)
-            driver.find_element_by_id("comboHidColumn").click()
+            driver.find_element_by_id("comboBoxXmlName").send_keys(v_meus[m])
             time.sleep(1)
+            driver.find_element_by_id("comboHidColumn").click()
+            time.sleep(3)
+            driver.find_element_by_id("comboHidColumn").click()
+            time.sleep(2)
+
             v_list = driver.find_elements_by_class_name("x-mcombo-text")
             for i in v_list:
-                # print(i.text)
-                if "分配规则" in i.text:
-                    i.click()
-                    time.sleep(1)
-                    # driver.find_element_by_id("comboHidColumn").click()
-                    # time.sleep(1)
-                    # 显示隐藏列
-                    driver.find_element_by_id("btnHidColumn").click()
-                    time.sleep(1)
-                    driver.find_element_by_id("btnSave").click()
-                    time.sleep(3)
-                    ClasForm.form_button_yes(self, "确定")
-                    break
-                else:
-                    pass
-                #     # 关闭自定义字段
-                #     driver.find_element_by_id("comboHidColumn").click()
-                #     driver.find_element_by_id("comboBoxXmlName").clear()
-                #     time.sleep(1)
-            n += 1
-
-
-
-            # v_check = driver.find_element_by_id("btnSelect")
-            # if v_check.is_displayed():
-            #     v_check.click()
-            # else:
-            #     driver.get_screenshot_as_file(root_path() + "TestPicture/Sys/test_1615_01_AddFPGZ.jpg")
-            #     unittest.expectedFailure("test_1615_01_AddFPGZ")
-
-    '''系统管理-常规设置-隐藏分配规则'''
-    def test_1615_02_HideFPGZ(self):
-        """系统管理-警报设置-数据相关查询"""
-        driver = self.driver
-        driver.find_element_by_id("comboBoxXmlName").clear()
-        n = 0
-        v_input = ['销售报价单(物料)', '销售报价单(服务)', '销售订单(物料)', '销售订单(服务)', "销售交货(物料)", "销售交货(服务)",
-                   "销售退货(物料)", "销售退货(服务)", '预收款申请(物料)', '预收款申请(服务)', '应收发票(物料)', '应收发票(服务)',
-                   '应收贷项凭证(物料)', '应收贷项凭证(服务)', '应收发票+付款(物料)', '应收预留发票(物料)', '应收预留发票(服务)',
-                   '采购申请(物料)', '采购申请(服务)', '采购报价单(物料)', '采购报价单(服务)', '采购订单(物料)', '采购订单(服务)',
-                   '采购收货(物料)', '采购收货(服务)', '采购退货(物料)', '采购退货(服务)', '应付发票(物料)', '应付发票(服务)',
-                   '预付款申请(物料)', '预付款申请(服务)', '应付贷项凭证(物料)', '应付贷项凭证(服务)', '应付预留发票(物料)',
-                   '应付预留发票(服务)']
-
-        while n < len(v_input):
-            driver.find_element_by_id("comboBoxXmlName").click()
-            driver.find_element_by_id("comboBoxXmlName").clear()
-            time.sleep(1)
-            driver.find_element_by_id("comboBoxXmlName").send_keys(v_input[n])
-            time.sleep(1)
-            v_compare = driver.find_element_by_class_name("x-combo-list-inner")
-            if v_compare.text == v_input[n]:
-                v_compare.click()
-            time.sleep(4)
-            driver.find_element_by_id("comboHidColumn").click()
-            time.sleep(1)
-            v_list = driver.find_elements_by_class_name("x-mcombo-text")
-            for i in v_list:
-                # print(i.text)
                 if "分配规则" == i.text:
                     i.click()
-                    time.sleep(1)
-                    # driver.find_element_by_id("comboHidColumn").click()
                     # time.sleep(1)
                     # 显示隐藏列
                     driver.find_element_by_id("btnHidColumn").click()
                     time.sleep(1)
                     driver.find_element_by_id("btnSave").click()
-                    time.sleep(3)
-                    ClasForm.form_button_yes(self, "确定")
+                    time.sleep(2)
+                    v_tip = driver.find_elements_by_class_name("ext-mb-text")
+                    # 提示
+                    for j in v_tip:
+                        try:
+                            "保存成功" in j.text
+                            ClasForm.form_button_yes(self, "确定")
+                            print(v_meus[m])
+                        except Exception as err:
+                            driver.get_screenshot_as_file(root_path() + "TestPicture/SYS/test_1615_01_AddFPGZ.jpg")
+                            print(err)
+                            unittest.expectedFailure("test_1615_01_AddFPGZ")
                     break
-                else:
-                    pass
-                #     # 关闭自定义字段
-                #     driver.find_element_by_id("comboHidColumn").click()
-                #     driver.find_element_by_id("comboBoxXmlName").clear()
-                #     time.sleep(1)
-            n += 1
+            m += 1
+
+    '''系统管理-常规设置-隐藏特定行字段'''
+
+    def test_1615_02_HideZD(self):
+        """系统管理-警报设置-数据相关查询"""
+        driver = self.driver
+        v_lists = driver.find_elements_by_class_name("x-grid3-hd-row")
+        fro
+        i in v_lists
+        v_meus = []
+
 
     def tearDown(self):
         self.driver.quit()
