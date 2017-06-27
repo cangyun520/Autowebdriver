f04160fca3088fb195b902ac6453550c46ba2a15
Administrator <online>
2017-06-27 14:33:05 +0800

    固定流批量配置

--- BaseDataSet/Int05_WorkFlow.py
+++ BaseDataSet/Int05_WorkFlow.py
@@ -91,5 +91,7 @@ class WorkFlow(unittest.TestCase):
     def tearDown(self):
         self.driver.quit()
         self.assertEqual([], self.verificationErrors)
+
+
 if __name__ == "__main__":
     unittest.main()
