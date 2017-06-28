81ab8e74ac64ea6d49f7aba7bca87e46b4aaeb9b
Administrator <online>
2017-06-28 15:15:27 +0800

    增加PC公共测试地址函数

--- RunTest/BD-RunTest.py
+++ RunTest/BD-RunTest.py
@@ -1,13 +1,8 @@
+
 from PubliCode.onlineClass import *
 
-'''修改当前测试URL地址'''
-url = "http://test.b1box.net"
-f = open(root_path() + 'PubliData/config/url.txt', 'w+')
-if f.readline() == url:
-    pass
-else:
-    f.write(url)
-    f.close()
+# 指定当前测试PC端地址
+root_pc_testurl("http://test.b1box.net")
 
 # 指定测试用例为当前文件夹下的test_case目录
 # 通过自定函数获取当前文件所在路径
